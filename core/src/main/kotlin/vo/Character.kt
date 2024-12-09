8316de298040feaf4465bff00394d64533c520c3
Mikhail Shadrin <shadrin.michael@yandex.ru>
2024-12-09 11:04:35 +0300

    Added template logic for hw3

--- core/src/main/kotlin/vo/Character.kt
+++ core/src/main/kotlin/vo/Character.kt
@@ -10,13 +10,30 @@ abstract class Character(
     baseAttack: Int,
     baseDefense: Int,
     var position: Position,
-    var direction: MoveDirection
+    var direction: MoveDirection,
+    level: Int = 1
 ) {
-    val maxHealth: Int = maxHealth.coerceAtLeast(1)
+    /**
+     * Максимальное количество здоровья
+     */
+    var maxHealth: Int = maxHealth.coerceAtLeast(1)
+        protected set
+
+    /**
+     * Текущее количество здоровья
+     *
+     * Не может опускаться ниже 0 и превышать [Character.maxHealth]
+     */
     var health: Int = maxHealth
         set(value) {
             field = value.coerceIn(0, maxHealth)
         }
+
+    /**
+     * Текущий уровень персонажа
+     */
+    var level: Int = level
+        protected set
 
     private val baseAttack: Int = baseAttack.coerceAtLeast(1)
     open val attack: Int get() = baseAttack
@@ -24,8 +41,17 @@ abstract class Character(
     private val baseDefense: Int = baseDefense.coerceAtLeast(1)
     open val defense: Int get() = baseDefense
 
+    /**
+     * Проверяет, жив ли персонаж
+     *
+     * @return `true`, если текущее здоровье больше 0; иначе `false`
+     */
     fun isAlive(): Boolean = health > 0
 
+    /**
+     * Атакует другого персонажа с учетом своего значения [Character.attack]
+     * и значения [Character.defense] противника
+     */
     fun hit(target: Character) {
         if (!isAlive()) {
             return
